9770c6bdca820a6cbedd34995873cad01e39f523
Pichau <igorof06@outlook.com>
2024-07-08 17:49:47 -0300

    readme.MD

--- magalums/src/main/java/com/challenge/magalums/scheduler/ScheduleTasks.java
+++ magalums/src/main/java/com/challenge/magalums/scheduler/ScheduleTasks.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
